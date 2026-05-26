14473a92fa2ec03a79c39529c58f19903c3d0e20
AiLaicNothing <ailaicnothing@gmail.com>
2026-05-26 15:06:31 -0500

    Update FireSlashSkill.cs

--- Dungeon/Assets/Scripts/Martin/Player/Skills/Base/Knight/FireSlashSkill.cs
+++ Dungeon/Assets/Scripts/Martin/Player/Skills/Base/Knight/FireSlashSkill.cs
@@ -23,24 +23,25 @@ public class FireSlashSkill : Skill
     public override void ServerExecute(PlayerController player, Vector3 targetPoint, Vector3 lockTargetPos)
     {
         Vector3 finalTarget = lockTargetPos != Vector3.zero? lockTargetPos : targetPoint;
-        
-        Vector3 dir =(finalTarget - player.PlayerModel.position).normalized;
-        dir.y = 0;
 
-        Vector3 spawnPos = player.PlayerModel.transform.position + dir * spawnOffset.z + Vector3.up * spawnOffset.y;
+        Vector3 spawnPos = player.PlayerModel.position + player.PlayerModel.forward * spawnOffset.z + Vector3.up * spawnOffset.y;
+
+        Vector3 dir = (finalTarget - spawnPos).normalized;
+
+        if (dir.sqrMagnitude < 0.0001f) dir = player.PlayerModel.forward;
 
         Quaternion rot = Quaternion.LookRotation(dir);
 
-        GameObject prefab = Instantiate(proyectilePrefab, spawnPos, rot);
-
+        GameObject prefab = Object.Instantiate(proyectilePrefab, spawnPos, rot);
         prefab.GetComponent<NetworkObject>().Spawn();
 
         PlayerProyectile proyectile = prefab.GetComponent<PlayerProyectile>();
 
-        if (proyectile != null )
+        if (proyectile != null)
         {
             proyectile.Initialize((player.Stats.PhysicalDamage.CurrentValue * hitData.physicalScale) + (player.Stats.MagicalDamage.CurrentValue * hitData.magicalScale), hitData, dir, proyectileSpeed, Vector3.zero);
         }
     }
-
 }
+
+
